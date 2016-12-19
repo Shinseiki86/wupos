83831988de39ee3ceca7957196b5d2f87e3ac099
shinseiki86 <shinseiki86@gmail.com>
2016-12-19 10:02:18 -0500

    Add cod agencia en dropdown

--- resources/views/certificados/edit.blade.php
+++ resources/views/certificados/edit.blade.php
@@ -77,7 +77,11 @@
 			<div class="col-md-6">
 				<select class="form-control" ng-model="selectedAgencia" id="AGEN_id" name="AGEN_id" ng-required="required" required>
 					<option value="" disabled>Seleccione una agencia</option>
-					<option value="{% agencia.AGEN_id %}" ng-repeat="agencia in arrAgencias | filter:filterRegi | orderBy:'AGEN_nombre'">{% agencia.AGEN_nombre %}</option>
+					<option
+						value="{% agencia.AGEN_id %}"
+						ng-repeat="agencia in arrAgencias | filter:filterRegi | orderBy:'AGEN_nombre'">
+						{% agencia.AGEN_id + ' - ' + agencia.AGEN_nombre %}
+					</option>
 				</select>
 				@if ($errors->has('AGEN_id'))
 					<span class="help-block">
